1677c3fa473eb7baf8edae8ed729ffe7cd603bd0
ArturRezende <arturrezende4@gmail.com>
2021-11-18 22:04:12 -0300

    c-main__leader finished is responsive

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+//main components
+import MainLeader from "./Main__Leader";
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+        <MainLeader />
+
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </main>
+    );
+  }
+}
+
+export default Main;
